a4eb29740bf35c69958f713deabee3ae08c80e8e
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-07-10 16:46:54 -0400

    modified mine/ to validate provided proof

--- basic_block_gp/blockchain.py
+++ basic_block_gp/blockchain.py
@@ -130,33 +130,29 @@ node_identifier = str(uuid4()).replace('-', '')
 blockchain = Blockchain()
 
 
-@app.route('/mine', methods=['GET'])
+@app.route('/mine', methods=['POST'])
 def mine():
+    miner = request.get_json()
     # We run the proof of work algorithm to get the next proof...
     last_block = blockchain.last_block
     last_proof = last_block['proof']
-    proof = blockchain.proof_of_work(last_proof)
+    proof_is_valid = blockchain.valid_proof(miner['proof'])
 
-    # We must receive a reward for finding the proof.
-    # TODO:
-    # The sender is "0" to signify that this node has mine a new coin
-    # The recipient is the current node, it did the mining!
-    # The amount is 1 coin as a reward for mining the next block
-    blockchain.new_transaction(0, node_identifier, 1)
+    response = {'message': 'Fail'}
 
-    # Forge the new Block by adding it to the chain
-    # TODO
+    if proof_is_valid:
+        blockchain.new_transaction(miner['id'], miner['wallet'], 1)
 
-    block = blockchain.new_block(proof, blockchain.hash(last_block))
+        block = blockchain.new_block(miner['proof'], blockchain.hash(last_block))
 
-    # Send a response with the new block
-    response = {
-        'message': "New Block Forged",
-        'index': block['index'],
-        'transactions': block['transactions'],
-        'proof': block['proof'],
-        'previous_hash': block['previous_hash'],
-    }
+        # Send a response with the new block
+        response = {
+            'message': "success",
+            'index': block['index'],
+            'transactions': block['transactions'],
+            'proof': block['proof'],
+            'previous_hash': block['previous_hash'],
+        }
     return jsonify(response), 200
 
 
